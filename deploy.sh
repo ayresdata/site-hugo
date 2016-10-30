b850f3d53a3c894f33dd9d1cccfbbf5a35e095ea
Emanuel Calvo <3manuek@esdebian.org>
2016-10-30 14:50:53 -0300

    pushed

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ sleep 0.5
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-hugo # -t "hugo-creative-theme" # if using a theme, replace by `hugo -t <yourtheme>`
+hugo -t ayres-theme # -t "hugo-creative-theme" # if using a theme, replace by `hugo -t <yourtheme>`
 
 # Go To Public folder
 cd public
